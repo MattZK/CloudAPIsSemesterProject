cd32bbd19a22d1d8104f49c81c1c95e9ca65b5be
Matthias Willemsen <me@mattwill.be>
2019-04-15 19:41:42 +0200

    Added a api credential save system

--- FourSquareAPIAngular/src/app/app.component.ts
+++ FourSquareAPIAngular/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'FourSquareAPIAngular';
+  @ViewChild('ClientID') ClientIDElement;
+  @ViewChild('ClientSecret') ClientSecretElement;
+
+  private APIKeyState: Boolean;
+
+  constructor(private dataService: DataService) {
+    this.APIKeyState = this.getAPICredentialsState();
+  }
+
+  setAPICredentials(event: Event) {
+    if (this.dataService.setAPICredentials(this.ClientIDElement.nativeElement.value, this.ClientSecretElement.nativeElement.value))
+      this.getAPICredentialsState();
+  }
+
+  getAPICredentialsState() {
+    return this.APIKeyState = this.dataService.APIKeyStatus();
+  }
 }
---
+++ FourSquareAPIAngular/src/app/data.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  private API_CLIENT: string;
+  private API_SECRET: string;
+
+  constructor() {
+    this.getAPICredentials();
+    console.log(this.API_CLIENT);
+    console.log(this.API_SECRET);
+  }
+
+  public APIKeyStatus() {
+    if (this.API_CLIENT && this.API_SECRET)
+      return true;
+    return false;
+  }
+
+  public setAPICredentials(CLIENTID: string, CLIENTSECRET: string) {
+    if (CLIENTID && CLIENTSECRET) {
+      localStorage.setItem('API_CLIENT', CLIENTID);
+      localStorage.setItem('API_SECRET', CLIENTSECRET);
+      this.getAPICredentials();
+      return true;
+    }
+    return false;
+  }
+
+  private getAPICredentials() {
+    this.API_CLIENT = localStorage.getItem('API_CLIENT');
+    this.API_SECRET = localStorage.getItem('API_SECRET');
+  }
+}
