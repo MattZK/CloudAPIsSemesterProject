875c735bc3c15b29343f17ecca065bb7da466c91
Matthias Willemsen <me@mattwill.be>
2019-04-15 21:13:19 +0200

    Added home & settings pages

--- FourSquareAPIAngular/src/app/app-routing.module.ts
+++ FourSquareAPIAngular/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+// Page Imports
+import { HomeComponent } from './pages/home/home.component';
+import { SettingsComponent } from './pages/settings/settings.component';
+
+const routes: Routes = [
+  { path: 'settings', component: SettingsComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '**', redirectTo: '/home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- FourSquareAPIAngular/src/app/app.module.ts
+++ FourSquareAPIAngular/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SettingsComponent } from './pages/settings/settings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
